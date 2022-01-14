2ec5d6073e3d6a7a238ce6d1c1176aa24e423c1b
miha-9111 <miha-9111@yandex.ru>
2022-01-14 04:56:20 +0300

    Building the Table

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./page-list.component.scss']
 })
 export class PageListComponent implements OnInit {
-  @Input() pages = [];
+  @Input() pages: any = [];
 
   constructor() { }
 
